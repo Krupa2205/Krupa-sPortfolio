f041018a931a844ff5c2578e309276df2b082c7c
Krupa22 <krupakaklotar22@gmail.com>
2025-02-10 15:52:14 +0530

    added profile public

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-// import logo from "../assets/img/logo.svg";
-// import profile from "../assets/img/Profile.jpg"
-import profile from "/src/assets/img/profile.jpg";  
 import linkedin from "../assets/img/linkedin.svg";
 import email from "../assets/img/email.svg";
 import github from "../assets/img/github.svg";
@@ -37,7 +34,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
         <Navbar.Brand href="/">
-  <img src={profile} alt="Profile" className="profile-img" />
+  <img src="/Profile.jpg" alt="Profile" className="profile-img" />
   <span className="brand-name"> Krupa Kaklotar </span>
 </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
